41e936b27bde420bff41b378c771e0e4eaeaa994
Arne Ehrlich <ehrlich@consider-it.de>
2016-12-21 16:05:32 +0100

    zsh hooks (urgent flag), dunst config, aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias ll='ls -GFhl' # Same as above, but in long listing format
 alias vi=nvim
 alias vim=nvim
 alias neovim=nvim
+alias vii3='$EDITOR "$HOME/.config/i3/config"&&i3-msg reload'
--- zsh/zsh_hooks.zsh
+++ zsh/zsh_hooks.zsh
@@ -2,7 +2,7 @@
 case $TERM in
   ( xterm* | rxvt*)
     function set_running_app { printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a" }
-    function precmd {
+    function set_title_tab   {
       # vcs_info
       # Put the string "hostname::/full/directory/path" in the title bar:
       echo -ne "\e]2;$PWD\a"
@@ -10,9 +10,46 @@ case $TERM in
       # Put the parentdir/currentdir in the tab
       echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
     }
-    function preexec { set_running_app }
-    function postexec { set_running_app }
     ;;
+  (*)
+    function set_running_app {}
+    function set_title_tab {}
 esac
 
+    function preexec { set_running_app }
+    function postexec { set_running_app }
+    function precmd {}
+
+
+
+
+function preexec {
+    # Note the date when the command started, in unix time.
+    CMD_START_DATE=$(date +%s)
+    # Store the command that we're running.
+    CMD_NAME=$1
+    set_running_app 
+}
+
+function postexec { set_running_app }
+
+function precmd {
+    # Proceed only if we've ran a command in the current shell.
+    if ! [[ -z $CMD_START_DATE ]]; then
+        # Note current date in unix time
+        CMD_END_DATE=$(date +%s)
+        # Store the difference between the last command start date vs. current date.
+        CMD_ELAPSED_TIME=$(($CMD_END_DATE - $CMD_START_DATE))
+        # Store an arbitrary threshold, in seconds.
+        CMD_NOTIFY_THRESHOLD=60
+
+        if [[ $CMD_ELAPSED_TIME -gt $CMD_NOTIFY_THRESHOLD ]]; then
+            # Beep or visual bell if the elapsed time (in seconds) is greater than threshold
+            print -n '\a'
+            # Send a notification
+            notify-send 'Job finished' "\"$CMD_NAME\" has finished."
+        fi
+    fi
+    set_title_tab
+}
 
