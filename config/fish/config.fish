5f6e76f5bda02a7b81f03a7593c10955e4f14297
Arne Ehrlich <ehrlich@consider-it.de>
2017-08-03 10:47:10 +0200

    fish: test for opam and virtualfish existence

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,10 +6,19 @@ set -gx PATH $HOME/.cargo/bin $PATH
 # source $HOME/kerl/19.2/activate.fish 
 # dynamic-colors init
 
-if test (id -u) -ne 0
-    # if normal user
-    eval (python -m virtualfish); or true
-    eval (opam config env); or true
-    source $HOME/.kiex/scripts/kiex.fish
+
+
+#the rest is for normal users, not for root
+if test (id -u) -eq 0
+    exit
 end
+
+if python -c 'import pkgutil; exit(0 if pkgutil.find_loader("virtualfish") else 1)'
+    eval (python -m virtualfish)
+end
+if which -s opan
+    eval (opam config env)
+end
+
+source $HOME/.kiex/scripts/kiex.fish
 
