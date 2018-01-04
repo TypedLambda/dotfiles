45ec85aced7115a6f4c6543f501c09f756ee0119
Arne Ehrlich <ehrlich@consider-it.de>
2018-01-04 10:54:57 +0100

    fish config cleanup, many small changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,26 +1,37 @@
-
 fish_vi_key_bindings
 
 # source $HOME/kerl/19.2/activate.fish 
 # dynamic-colors init
-
-
 
 #the rest is for normal users, not for root
 if test (id -u) -eq 0
     exit
 end
 
-if python -c 'import pkgutil; exit(0 if pkgutil.find_loader("virtualfish") else 1)'
-    eval (python -m virtualfish)
+function export_prepend_dir -d 'prepend a path to a variable if it exists'
+    if test -d $argv[1]
+        set -gx $argv[1] $argv[2..-1] $$argv[1]
+    end
 end
+
+function source_existing -d 'source file if it exists'
+    if test -f "$argv"
+        source "$argv"
+    end
+end
+
+if which -s python
+    if python -c 'import pkgutil; exit(0 if pkgutil.find_loader("virtualfish") else 1)'
+        eval (python -m virtualfish)
+    end
+end
+
 if which -s opan
     eval (opam config env)
 end
 
 # set -gx PATH /usr/local/lib/erlang20/bin $PATH
-set -gx PATH "$HOME/.node/bin" $PATH
-set -gx PATH "$HOME/.cargo/bin" $PATH
-set -gx NODE_PATH "$HOME/.node/lib/node_modulesr" $NODE_PATH 
-
-source "$HOME/.asdf/asdf.fish"
+export_prepend_dir PATH  "$HOME/.node/bin"
+export_prepend_dir PATH "$HOME/.cargo/bin"
+export_prepend_dir NODE_PATH "$HOME/.node/lib/node_modules" 
+source_existing "$HOME/.asdf/asdf.fish"
---
+++ config/fish/functions/fish_greeting.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.KkbwyC/fish_greeting.fish @ line 2
+function fish_greeting
+	set_color -i blue
+	fortune freebsd-tips
+	set_color normal
+end
