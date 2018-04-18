6d317ea103d46c7f77751bd3db927a1870ff4ac0
Arne Ehrlich <ehrlich@consider-it.de>
2018-04-18 10:30:57 +0200

    screenlayout script update

--- screenlayout/eDP1310x1440HDMI1600x1440.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 2560x1440 --pos 0x320 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2 --off
